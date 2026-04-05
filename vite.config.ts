4034c09f5fb23d5ca391828c8b0cf6785dd8ef1c
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-04-05 14:00:16 +0300

    add ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // корень
+  base: '/react-v3-blended-starter/', // обязательно для GitHub Pages
   plugins: [react()],
 });
