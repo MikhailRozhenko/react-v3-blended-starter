8d71398c889a04113981f3eaa2582385098e76b1
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-04-05 13:53:09 +0300

    add verceljson

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // для Vercel сайт на корне
+  base: '/', // корень
   plugins: [react()],
 });
